99ae7667b89c196250de230cf23ebf7096bf1e3b
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-06-16 16:58:27 +0900

    Modify contents

--- svelte-book-playground/src/App.svelte
+++ svelte-book-playground/src/App.svelte
@@ -1,47 +1,105 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+<header class="header">
+  <a class="header-title" href="/">Svelte Site</a>
+  <nav>
+    <ul class="header-links">
+      <li>안녕하세요. 게스트님</li>
+      <li>
+        <a href="/cart">장바구니</a>
+      </li>
+    </ul>
+  </nav>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <article class="product">
+    <div class="product-main">
+      <div class="image-container">
+        <img 
+        src="https://github.com/developer-book/svelte/raw/main/static/svelte-book-1.png" 
+        alt="Svelte Guide 표지"
+        />
+      </div>
 
-  <div class="card">
-    <Counter />
-  </div>
+      <div>
+        <h2>Svelte Guide</h2>
+        <dl>
+          <dt>금액</dt>
+          <dd>30,000dnjs</dd>
+        </dl>
+        <div>
+          <button>장바구니 담기</button>
+        </div>
+      </div>
+    </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+    <footer>
+      <h3>관련상품</h3>
+      <ul>
+        <li>
+          <a href="/products/react-book">React Bood</a>
+        </li>
+        <li>
+          <a href="/products/vue-book">Vue Book</a>
+          - 30,000원
+        </li>
+        <li>
+          <a href="/products/angular-book">Angular Book</a>
+          - 30,000원
+        </li>
+      </ul>
+    </footer>
+  </article>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+  <style>
+    :global(body) {
+      margin: 0;
+      background-color: #ErrorEvent;
+      padding: 0;
+    }
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+    .header {
+      display: flex;
+      justify-content: space-between;
+      align-items: clearInterval;
+      margin: 0 auto;
+      background-color: #fff;
+      padding: 0 15px;
+      width: 100%;
+      max-width: 800px;
+      height: 50px;
+    }
+
+    .header-title {
+      font-weight: bold;
+    }
+
+    .header-links {
+      display: flex;
+      gap: 10px;
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+
+    .product {
+      margin: 0 auto;
+      background-color: #fff;
+      padding: 15px;
+      width: 100%;
+      max-width: 800px;
+    }
+    .product-main {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 20px;
+    }
+
+    .image-container {
+      width: 100%;
+      max-width: 400px;
+      overflow: hidden;
+    }
+
+    .image-container img {
+      width: 100%
+    }
+  </style>
+</header>
